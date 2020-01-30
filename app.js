65cd412328e2a880bc1ebbccb66368aefc7e4152
Ivan Voynov <ivanvoynov@hotmail.com>
2020-01-30 17:22:20 -0500

    Add custom middleware

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 // Imports
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 // Our express application
 const app = express();
 
 // Middleware
+app.use(morgan('dev'));
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware! 👋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// Route handlers
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
+    requestTime: req.requestTime,
     results: tours.length,
     data: {
       tours
