a27346a78074b528543f682d9586b99e39a9b7df
Ivan Voynov <ivanvoynov@hotmail.com>
2020-01-30 16:40:01 -0500

    Add tour by id get method

--- app.js
+++ app.js
@@ -23,6 +23,26 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = +req.params.id;
+  const tour = tours.find(el => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  });
+});
+
 // POST a tour
 app.post('/api/v1/tours/', (req, res) => {
   // console.log(req.body);
