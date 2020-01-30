77a43444bf6f52db73f307d16154560e57131606
Ivan Voynov <ivanvoynov@hotmail.com>
2020-01-30 15:27:00 -0500

    Add get and post methods for api

---
+++ app.js
@@ -0,0 +1,52 @@
+// Imports
+const fs = require('fs');
+const express = require('express');
+
+// Our express application
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+// GET all tours
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+});
+
+// POST a tour
+app.post('/api/v1/tours/', (req, res) => {
+  // console.log(req.body);
+
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
+});
+
+// Configure our server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
