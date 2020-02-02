406d9343581efe75fde4185a5a35bd4be6c2ddc7
Ivan Voynov <ivanvoynov@hotmail.com>
2020-02-02 12:06:18 -0500

    Add user model

---
+++ models/userModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name'],
+    trim: true
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must provide an email'],
+    unique: [
+      true,
+      'Email {VALUE} is already taken. Please use another email instead.'
+    ],
+    lowercase: true,
+    trim: true,
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide a valid email'
+    }
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password']
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
