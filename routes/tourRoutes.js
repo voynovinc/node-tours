885a2f09fd417a6d0d663aeb51af09a67b89cdf2
Ivan Voynov <ivanvoynov@hotmail.com>
2020-02-04 15:30:13 -0500

    Add nested review routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
+const reviewRouter = require('./../routes/reviewRoutes');
 const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
