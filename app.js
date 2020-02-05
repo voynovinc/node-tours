d808a9e8fa93e315ca054a403a59a9f7c2ad4f64
Ivan Voynov <ivanvoynov@hotmail.com>
2020-02-05 12:15:44 -0500

    Set up pug template engine

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -15,8 +16,13 @@ const reviewRouter = require('./routes/reviewRoutes');
 // Our express application
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // MIDDLEWARE STACK
 // Set security http
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet());
 
 // Development logging
@@ -55,9 +61,6 @@ app.use(
   })
 );
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // Testing middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -65,6 +68,10 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
