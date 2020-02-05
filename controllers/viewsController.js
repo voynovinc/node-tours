9e26c7a4589f8174107b974fa38a6d82b8a9b69a
Ivan Voynov <ivanvoynov@hotmail.com>
2020-02-05 18:45:55 -0500

    Build tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,11 +1,29 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All tours'
-  });
-};
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utlis/catchAsync');
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1) get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) build template
+
+  // 3) render that template using tour data from 1)
+
+  res.status(200).render('overview', {
+    title: 'All tours',
+    tours
   });
-};
+});
+
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) get data
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
+  });
+
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+    tour
+  });
+});
