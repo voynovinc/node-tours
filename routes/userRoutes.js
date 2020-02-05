4da181f9f27a89cc2be60c7e455449d6771da53a
Ivan Voynov <ivanvoynov@hotmail.com>
2020-02-04 19:46:23 -0500

    Specify authorized user restrictions for each route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,19 +6,18 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
+// Protect all further routes from unauthorized users
+router.use(authController.protect);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+router.patch('/updateMyPassword', authController.updatePassword);
 
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
-
+// Only admins should perform these actions
+router.use(authController.restrictTo('admin'));
 router
   .route('/')
   .get(userController.getAllUsers)
